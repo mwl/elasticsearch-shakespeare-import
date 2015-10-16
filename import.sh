9671335de7965cf04c36f48a4affc9d9108779bd
Martin Westergaard Lassen <martin@mwl.dk>
2015-10-16 15:56:27 +0100

    Wrong filename

--- import.sh
+++ import.sh
@@ -4,7 +4,7 @@ echo -n "Deleting data:     "
 curl -XDELETE -s ${ELASTIC_SEARCH_URL}/shakespeare && sleep 3 && echo
 
 echo -n "Configuring index: "
-curl -XPUT -s -H'Content-Type: application/json' --data-binary @setup.json ${ELASTIC_SEARCH_URL}/shakespeare && sleep 3 && echo
+curl -XPUT -s -H'Content-Type: application/json' --data-binary @index.json ${ELASTIC_SEARCH_URL}/shakespeare && sleep 3 && echo
 
 echo -n "Importing data     "
 curl -XPUT -s -H'Content-Type: application/json' --data-binary @shakespeare.json ${ELASTIC_SEARCH_URL}/_bulk > /dev/null || (echo "FAILED"; exit 1) && echo "Success"
